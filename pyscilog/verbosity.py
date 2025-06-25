a2b6bf2869e24c807f3d181d2b5338195f378db4
landmanbester <lbester@sarao.ac.za>
2025-06-25 15:41:37 +0200

    fix mypy errors and reduce testing matrix

--- pyscilog/verbosity.py
+++ pyscilog/verbosity.py
@@ -1,27 +1,31 @@
 import re
-from typing import Union, List
+from typing import Union, List, Any
 from pyscilog import get_logger
 from pyscilog.state import State
 
 state = State()
 
 
-def set_verbosity(verbosity: Union[int, List[int], str]):
+def set_verbosity(verbosity: Union[int, List[int], str, None]):
     if verbosity is None:
         state['verbosity'] = 0
         return
     # ensure verbosity is turned into a list.
+    verbosity_list: List[Any]
     if type(verbosity) is int:
-        verbosity = [verbosity]
+        verbosity_list = [verbosity]
     elif isinstance(verbosity, str):
-        verbosity = verbosity.split(",")
-    elif not isinstance(verbosity, (list, tuple)):
+        verbosity_list = verbosity.split(",")
+    elif isinstance(verbosity, (list, tuple)):
+        verbosity_list = list(verbosity)
+    else:
         raise TypeError("can't parse verbosity specification of type '{}'".format(type(verbosity)))
-    for element in verbosity:
-        if type(element) is int or re.match("^[0-9]+$", element):
+    
+    for element in verbosity_list:
+        if type(element) is int or (isinstance(element, str) and re.match("^[0-9]+$", element)):
             state['verbosity'] = int(element)
             state['log'](0, "green").print("set global console verbosity level {}".format(state['verbosity']))
-        else:
+        elif isinstance(element, str):
             m = re.match("^(.+)=([0-9]+)$", element)
             if not m:
                 raise ValueError("can't parse verbosity specification '{}'".format(element))
@@ -31,23 +35,27 @@ def set_verbosity(verbosity: Union[int, List[int], str]):
             logger(0, "green").print("set console verbosity level {}={}".format(m.group(1), level))
 
 
-def get_verbosity(verbosity: Union[List[int], int, str]):
+def get_verbosity(verbosity: Union[List[int], int, str, None]):
     if verbosity is None:
         state['log_verbosity'] = None  # None means follow console default
         return
     # ensure verbosity is turned into a list.
+    verbosity_list: List[Any]
     if type(verbosity) is int:
-        verbosity = [verbosity]
+        verbosity_list = [verbosity]
     elif isinstance(verbosity, str):
-        verbosity = verbosity.split(",")
-    elif not isinstance(verbosity, (list, tuple)):
+        verbosity_list = verbosity.split(",")
+    elif isinstance(verbosity, (list, tuple)):
+        verbosity_list = list(verbosity)
+    else:
         raise TypeError("can't parse verbosity specification of type '{}'".format(type(verbosity)))
-    for element in verbosity:
-        if type(element) is int or re.match("^[0-9]+$", element):
+    
+    for element in verbosity_list:
+        if type(element) is int or (isinstance(element, str) and re.match("^[0-9]+$", element)):
             state['log_verbosity'] = int(element)
             if state['log_verbosity'] is not None:
                 state['log'](0, "green").print("set global log verbosity level {}".format(state['log_verbosity']))
-        else:
+        elif isinstance(element, str):
             m = re.match("^(.+)=([0-9]+)$", element)
             if not m:
                 raise ValueError("can't parse verbosity specification '{}'".format(element))
